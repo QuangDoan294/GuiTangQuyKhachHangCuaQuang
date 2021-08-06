928192362d7b8fb882231654fe69723c36ca1ce2
QuangDoan294 <88539168+QuangDoan294@users.noreply.github.com>
2021-08-06 19:36:34 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -22,9 +22,9 @@ function firstQuestion(){
     Swal.fire({
         title: CONFIG.introTitle,
         text: CONFIG.introDesc,
-        imageUrl: 'img/quanghihi.jpg',
-        imageWidth: 735,
-        imageHeight: 797,
+        imageUrl: 'img/lookMe.jpg',
+        imageWidth: 300,
+        imageHeight: 300,
         background: '#fff url("img/iput-bg.jpg")',
         imageAlt: 'Custom image',
         confirmButtonText: CONFIG.btnIntro
