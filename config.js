8ac827b01635b1dbc67567737c8c003885ba217e
QuangDoan294 <88539168+QuangDoan294@users.noreply.github.com>
2021-08-07 08:33:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
-    introTitle: 'Chào Ngân!',
+    introTitle: 'Chào Hiền!',
     introDesc: `Dịch bệnh Covid đang diễn biến khó lường. Bạn hãy giữ gìn sức khỏe và cùng cộng đồng phòng chống dịch nhé`,
     btnIntro: 'Tiếp tục...',
-    title: 'Hãy luôn thực hiện tốt chỉ thị 5K để giữ gìn sức khỏe cho chính mình và cộng đồng Ngân nhé',
+    title: 'Hãy luôn thực hiện tốt chỉ thị 5K để giữ gìn sức khỏe cho chính mình và cộng đồng Hiền nhé',
     desc: '...',
     btnYes: 'Đó là điều chắc chắn rồi :)))',
     btnNo: 'Tôi sẽ không làm điều đó đâu :(((',
     question:'Hãy gửi một thông điệp gì đó cho mọi người đi nào...',
     btnReply: 'Gửi đi thông điệp',
-    reply: 'Mọi người hãy cùng Ngân chung tay đẩy lùi đại dịch Covid19 này nhé!...........',
+    reply: 'Mọi người hãy cùng Hiền chung tay đẩy lùi đại dịch Covid19 này nhé!...........',
     mess: 'Đó là điều chắc chắn rồi :))))',
-    messDesc: 'Chúc Ngân một ngày mới tràn đầy năng lượng nhé. Khi cần giải pháp bảo vệ hãy liên hệ Quang nhé. SĐT nè: 0904321312!',
+    messDesc: 'Chúc Hiền một ngày mới tràn đầy năng lượng nhé. Khi cần giải pháp bảo vệ hãy liên hệ Quang nhé. SĐT nè: 0904321312!',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
