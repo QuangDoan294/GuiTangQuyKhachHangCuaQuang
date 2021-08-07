de3de000343cb79f02e359bd3de594febe12408c
QuangDoan294 <88539168+QuangDoan294@users.noreply.github.com>
2021-08-07 07:05:30 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -23,8 +23,8 @@ function firstQuestion(){
         title: CONFIG.introTitle,
         text: CONFIG.introDesc,
         imageUrl: 'img/5k.jpg',
-        imageWidth: 300,
-        imageHeight: 400,
+        imageWidth: 500,
+        imageHeight: 700,
         background: '#fff url("img/iput-bg.jpg")',
         imageAlt: 'Custom image',
         confirmButtonText: CONFIG.btnIntro
